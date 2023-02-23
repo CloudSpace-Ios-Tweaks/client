abcd66b3dda77d597c4c4440aa477b93337e1b56
piggydoe <67165527+piggydoe@users.noreply.github.com>
2023-02-22 21:06:42 -0800

    woops

--- src/routes/jailbreaks/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { App, Page, Navbar, Block } from 'konsta/svelte';
-    import AppComp from '../components/AppComp.svelte';
-    import Apps from '../state/apps.svelte';
-</script>t
-
-<Block outline>
-    <Apps type="jailbreaks"/>
-</Block>
--- src/routes/tweaks/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { App, Page, Navbar, Block } from 'konsta/svelte';
-    import AppComp from '../components/AppComp.svelte';
-    import Apps from '../state/apps.svelte';
-</script>
-
-<Block outline>
-    <Apps type="tweaks"/>
-</Block>

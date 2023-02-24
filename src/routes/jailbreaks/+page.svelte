ef8f8e8c2d270906b1d831f69228073628b07529
piggydoe <67165527+piggydoe@users.noreply.github.com>
2023-02-24 09:46:03 -0800

    Update +page.svelte

--- src/routes/jailbreaks/+page.svelte
+++ src/routes/jailbreaks/+page.svelte
@@ -2,7 +2,7 @@
     import { App, Page, Navbar, Block } from 'konsta/svelte';
     import AppComp from '../components/AppComp.svelte';
     import Apps from '../state/apps.svelte';
-</script>t
+</script>
 
 <Block outline>
     <Apps type="jailbreaks"/>
